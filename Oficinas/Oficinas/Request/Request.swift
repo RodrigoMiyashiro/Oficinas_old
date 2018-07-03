348e55cfab9ec16a0c2240879684cbd66b19953e
Rodrigo Miyashiro <rodrigo.miyashiro@yahoo.com.br>
2018-07-03 01:40:14 -0300

    Some archives added.

---
+++ Oficinas/Oficinas/Request/Request.swift
@@ -0,0 +1,37 @@
+//
+//  Request.swift
+//  Oficinas
+//
+//  Created by Rodrigo Miyashiro on 03/07/18.
+//  Copyright © 2018 Rodrigo Miyashiro. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class Request: NSObject
+{
+    static func getFrom(_ url: String, _ parameters: [String: Any]? = nil, headers: [String:String]? = nil ,  completion: @escaping(_ result: Any?, _ error: Any?) -> Void)
+    {
+        Alamofire.request(url.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) ?? url, method: .get, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
+            .validate(statusCode: 200..<300)
+            .responseJSON { (response) in
+                print("GET: \(response.request?.url?.absoluteString ?? "")")
+                
+                switch response.result {
+                case .success:
+                    completion(response.data, nil)
+                    break
+                    
+                case .failure(let error):
+                    if let errorResponse = response.data {
+                        completion(nil, errorResponse)
+                    } else {
+                        completion(nil, error)
+                    }
+                    break
+                }
+        }
+    }
+}
---
+++ Oficinas/Oficinas/Utils/Generate URL/GeneratorURL.swift
@@ -0,0 +1,9 @@
+//
+//  GeneratorURL.swift
+//  Oficinas
+//
+//  Created by Rodrigo Miyashiro on 03/07/18.
+//  Copyright © 2018 Rodrigo Miyashiro. All rights reserved.
+//
+
+import Foundation
